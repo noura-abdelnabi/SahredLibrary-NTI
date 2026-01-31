2ab3f354b71e301b4b3bebfabf75dcdcb33e1ac6
Noura Abdelnabi <noura.abdelnabi.hassan@gmail.com>
2026-01-31 22:05:26 +0200

    Update buildAndPushImage.groovy

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -5,14 +5,14 @@ def call(String imageName, String dockerhubCredID) {
     echo "--- Phase: Scanning Docker Image: ${imageName} ---"
     sh "trivy image --severity HIGH,CRITICAL --timeout 20m ${imageName}"
     
-    //echo "--- Pushing Image to Docker Hub ---"
-    //withCredentials([usernamePassword(credentialsId: dockerhubCredID, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        //sh """
-            //docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
-            //docker push ${imageName}
-        //"""
-    //}
+    echo "--- Pushing Image to Docker Hub ---"
+    withCredentials([usernamePassword(credentialsId: dockerhubCredID, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+            docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
+            docker push ${imageName}
+        """
+    }
 
-    //echo "--- Removing Local Image to save space ---"
-    //sh "docker rmi ${imageName}"
+    echo "--- Removing Local Image to save space ---"
+    sh "docker rmi ${imageName}"
 }
